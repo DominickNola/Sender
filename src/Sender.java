3bc5a177c25a01b44ee391fdf7c20e1e124939b6
Dominick Licciardi <dominick.nola@gmail.com>
2019-10-08 20:04:30 -0600

    works yay

--- src/Sender.java
+++ src/Sender.java
@@ -3,7 +3,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.*;
 import java.io.*;
@@ -12,7 +11,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Sender {
@@ -27,21 +26,27 @@ public class Sender {
 
     public static void main(String[] args) throws Exception {
 
+        String inputFileName;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input the name of the message file: ");
+        inputFileName = scan.nextLine();
+        scan.close();
+
         // **** SHA/RSA ****
-        sha256("message.txt");
+        sha256(inputFileName);
         // Number 5, String from message.dd to byte[] for RSA/Kx- Encryption
-        stringToByte("message.dd");
+        //stringToByte("message.dd");
         // Number 5, RSA Encryption using Kx- Key.
-        rsaEncrypt();
+        rsaEncrypt(inputFileName);
 
         // **** AES ****
         keyToUTF8("symmetric.key");
-        readDsMsg("message.ds-msg");
-        aesEncrypt();
+        //readDsMsg("message.ds-msg");
+        aesEncrypt("message.ds-msg");
 
     }
 
-    public static String sha256(String message_file) throws Exception {
+    public static void sha256(String message_file) throws Exception {
 
         BufferedInputStream file = new BufferedInputStream(new FileInputStream(message_file));
         MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -55,58 +60,25 @@ public class Sender {
         in.close();
 
         byte[] hash = md.digest();
-        PrintWriter out = new PrintWriter("message.dd");
+        hashByte = hash;
+        BufferedOutputStream dd_out = new BufferedOutputStream(new FileOutputStream("message.dd"));
         System.out.println("SHA256(M) in Hexadecimal bytes, output to message.dd:");
         for (int k = 0, j = 0; k < hash.length; k++, j++) {
             System.out.format("%02X ", (hash[k])) ;
             // save value to message.dd file
-            out.format("%02X ", (hash[k]));
+            dd_out.write(hash[k]);
             if (j >= 15) {
                 System.out.println("");
                 j = -1;
             }
         }
+
+        System.out.println(hash);
+        //dd_out.write(hash);
         System.out.println("");
-        out.close();
+        dd_out.close();
 
-        return new String(hash);
-    }
-
-    public static String stringToByte(String fileName) throws IOException {
-
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-
-            messageHash = sb.toString();
-            messageHash = messageHash.replaceAll("\\s+","");
-
-            // Convert Hex String to byte Array.
-            hashByte = new byte[messageHash.length() / 2];
-            for (int i = 0; i < hashByte.length; i++) {
-                int index = i * 2;
-                int j = Integer.parseInt(messageHash.substring(index, index + 2), 16);
-                hashByte[i] = (byte)j;
-            }
-
-            System.out.println("byte[] hashByte: (Read message.dd as a string and store as byte[].) ");
-            for (int i = 0; i < hashByte.length; i++) {
-                System.out.format("%02X ", (hashByte[i]));
-            }
-            System.out.println();
-            System.out.println();
-
-            return sb.toString();
-        } finally {
-            br.close();
-        }
+        //return new String(hash);
     }
 
     //read key parameters from a file and generate the private key
@@ -145,17 +117,17 @@ public class Sender {
 
             while (line != null) {
                 sb.append(line);
-                sb.append("\n");
+                //sb.append("\n");
                 line = br.readLine();
             }
             symmetricKey = sb.toString();
-            System.out.print(symmetricKey);
+            System.out.print(symmetricKey + "\n");
             return sb.toString();
         } finally {
             br.close();
             System.out.println("128-bit UTF-8 encoding of Symmetric.key for AES: ");
             symmetricBytes = symmetricKey.getBytes("UTF-8");
-            symmetricBytes = trim(symmetricBytes);
+            //symmetricBytes = trim(symmetricBytes);
             for (byte x: symmetricBytes) {
                 System.out.print(x + " ");
             }
@@ -163,76 +135,82 @@ public class Sender {
         }
     }
 
-    static byte[] trim(byte[] bytes) {
-        int i = bytes.length - 1;
-        while (i >= 0 && bytes[i] == 0)
-            --i;
-        return Arrays.copyOf(bytes, i );
-    }
-
-    public static byte[] rsaEncrypt() throws Exception {
+    public static byte[] rsaEncrypt(String messageInputFile) throws Exception {
 
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        SecureRandom Xrandom = new SecureRandom();
         XprivKey2 = readPrivKeyFromFile("XPrivate.key");
-        cipher.init(Cipher.ENCRYPT_MODE, XprivKey2, Xrandom);
-        PrintWriter out = new PrintWriter("message.ds-msg");
+        cipher.init(Cipher.ENCRYPT_MODE, XprivKey2);
+
+        BufferedOutputStream dsMsg_out = new BufferedOutputStream(new FileOutputStream("message.ds-msg"));
         byte[] cipherText = cipher.doFinal(hashByte);
         System.out.println("RSA Encryption cipherText: block size = " + cipher.getBlockSize());
         for (int i = 0, j = 0; i < cipherText.length; i++, j++) {
             System.out.format("%02X ", (cipherText[i]));
-            out.format("%02X ", (cipherText[i]));
+            //out.format("%02X ", (cipherText[i]));
             if (j >= 15) {
                 System.out.println("");
                 j = -1;
             }
         }
-        // read from the original message message.txt and append to message.ds-msg
-        try(BufferedReader br = new BufferedReader(new FileReader("message.txt"))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            String orig_message = sb.toString();
-            out.print(orig_message);
-        }
-        System.out.println("");
-        out.close(); // IT WONT SEND THE DATA WITHOUT THIS LINE
+        dsMsg_out.write(cipherText);
+
+        BufferedInputStream messageFile = new BufferedInputStream(new FileInputStream(messageInputFile));
+        byte[] orig_message = messageFile.readAllBytes();
+        messageFile.close();
+        // // read from the original message message.txt and append to message.ds-msg
+        // try(BufferedReader br = new BufferedReader(new FileReader(messageInputFile))) {
+        //     StringBuilder sb = new StringBuilder();
+        //     String line = br.readLine();
+        //     while (line != null) {
+        //         sb.append(line);
+        //         sb.append(System.lineSeparator());
+        //         line = br.readLine();
+        //     }
+        //     orig_message = sb.toString();
+        //     //out.print(orig_message);
+        // }
+        dsMsg_out.write(orig_message);
+        System.out.println();
+        dsMsg_out.close(); // IT WONT SEND THE DATA WITHOUT THIS LINE
 
         return cipher.doFinal(hashByte);
     }
 
-    public static byte[] aesEncrypt() throws Exception {
+    public static void aesEncrypt(String dsMsg) throws Exception {
+
+        // Reading file as bytes
+        BufferedInputStream encryptedIn = new BufferedInputStream(new FileInputStream("message.ds-msg"));
+        byte[] RSABytes = encryptedIn.readAllBytes();
+        encryptedIn.close();
 
         byte[] iv = new byte[16];
-        String IV = "AAAAAAAAAAAAAAAA"; // do not need for AES/ECB/PKCS5Padding mode
+        //String IV = "AAAAAAAAAAAAAAAA"; // do not need for AES/ECB/PKCS5Padding mode
         //Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
         //Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding", "SunJCE");
         Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding", "SunJCE");
         SecretKeySpec key = new SecretKeySpec(symmetricBytes, "AES");
         cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(iv));
-        PrintWriter aes_out = new PrintWriter("message.aescipher");
-        byte[] AEScipher = cipher.doFinal(dsMsg.getBytes("UTF-8"));
-        System.out.print("AEScipher:  \n");
+
+        BufferedOutputStream aes_out = new BufferedOutputStream(new FileOutputStream("message.aescipher"));
+        byte[] AEScipher = cipher.doFinal(RSABytes);
+        System.out.print("AEScipher Encryption:  \n");
         for (int i = 0, j = 0; i < AEScipher.length; i++, j++) {
             System.out.format("%02X ", AEScipher[i]);
-            aes_out.format("%02X ", AEScipher[i]);
+            //aes_out.format("%02X ", AEScipher[i]);
             if (j >= 15) {
                 System.out.println("");
                 j = -1;
             }
         }
         System.out.println();
+        aes_out.write(AEScipher);
         aes_out.close();
-        return cipher.doFinal(dsMsg.getBytes("UTF-8"));
+        //return cipher.doFinal(dsMsg.getBytes("UTF-8"));
     }
 
     public static String readDsMsg(String fileName) throws IOException {
 
-        System.out.println("Read (RSA Cipertext || Message) string from message.ds-msg for AES Encryption: ");
+        System.out.println("Read (RSA Cipertext || Message) byte[] from message.ds-msg for AES Encryption: ");
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         try {
             StringBuilder sb = new StringBuilder();
